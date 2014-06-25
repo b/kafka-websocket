ad9987e750ead2cf4a7e6ceb2c03db075bf7ea39
b <b@b3k.us>
2014-06-25 16:22:21 -0700

    Introduce AbstractMessage to allow more flexibility for transforms

---
+++ src/main/java/us/b3k/kafka/ws/messages/AbstractMessage.java
@@ -0,0 +1,18 @@
+package us.b3k.kafka.ws.messages;
+
+public abstract class AbstractMessage {
+    protected String topic;
+
+    public abstract Boolean isKeyed();
+    public abstract byte[] getMessageBytes();
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public abstract String getKey();
+}
